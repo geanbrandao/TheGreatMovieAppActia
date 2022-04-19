f780a0851dea001ada4d9b1aea9f393adc2a20e1
gean.brandao <karlos867gean@gmail.com>
2022-04-19 20:45:14 -0300

    Create navigation between screen

--- app/src/main/java/br/com/geanbrandao/thegreatmovieapp/actia/presentation/movieDetails/MovieDetailsFragment.kt
+++ app/src/main/java/br/com/geanbrandao/thegreatmovieapp/actia/presentation/movieDetails/MovieDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.geanbrandao.thegreatmovieapp.actia.databinding.FragmentMovieDetailsBinding
 
 class MovieDetailsFragment : Fragment() {
@@ -19,5 +20,16 @@ class MovieDetailsFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        createListeners()
+    }
+
+    private fun createListeners() {
+        binding.componentToolbar.ivBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
--- app/src/main/java/br/com/geanbrandao/thegreatmovieapp/actia/presentation/movies/MoviesFragment.kt
+++ app/src/main/java/br/com/geanbrandao/thegreatmovieapp/actia/presentation/movies/MoviesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.geanbrandao.thegreatmovieapp.actia.R
 import br.com.geanbrandao.thegreatmovieapp.actia.databinding.FragmentMoviesBinding
 
@@ -23,4 +24,14 @@ class MoviesFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        createListeners()
+    }
+
+    private fun createListeners() {
+        binding.tvHeader.setOnClickListener {
+            findNavController().navigate(R.id.action_moviesFragment_to_movieDetailsFragment)
+        }
+    }
 }
