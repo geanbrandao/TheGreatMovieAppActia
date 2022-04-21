c184fb3eeeb49be2cfb5e5857ae618ad9e371d08
gean.brandao <karlos867gean@gmail.com>
2022-04-21 16:49:23 -0300

    Add job to cancel if screen is finish

--- app/src/main/java/br/com/geanbrandao/thegreatmovieapp/actia/presentation/movies/MoviesViewModel.kt
+++ app/src/main/java/br/com/geanbrandao/thegreatmovieapp/actia/presentation/movies/MoviesViewModel.kt
@@ -3,11 +3,11 @@ package br.com.geanbrandao.thegreatmovieapp.actia.presentation.movies
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.geanbrandao.thegreatmovieapp.actia.domain.model.DiscoverModel
-import br.com.geanbrandao.thegreatmovieapp.actia.domain.useCases.DiscoverMoviesUseCase
 import br.com.geanbrandao.thegreatmovieapp.actia.domain.useCases.MoviesUseCase
 import br.com.geanbrandao.thegreatmovieapp.actia.utils.State
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -17,6 +17,7 @@ class MoviesViewModel @Inject constructor(
     private val moviesUseCase: MoviesUseCase
 ): ViewModel() {
 
+    private var job: Job? = null
     val movies = MutableStateFlow<State<DiscoverModel>>(State.LoadingState(isLoading = true))
 
     fun getDiscoverMovies() {
@@ -24,5 +25,10 @@ class MoviesViewModel @Inject constructor(
             .onEach {
                 movies.value = it
             }.launchIn(viewModelScope)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
     }
 }
